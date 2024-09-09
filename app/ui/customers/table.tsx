03fd6f238da5934633f15c5dc171e7fc65dba71a
mesfin-2 <mesfintdev@gmail.com>
2024-09-09 13:17:30 +0300

    first commit

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,8 +1,8 @@
 import {
   FormattedCustomersTable
 } from '@/app/lib/definitions';
-import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
+import { lusitana } from 'app/ui/fonts';
 import Image from 'next/image';
 
 export default async function CustomersTable({
